92d35fb0326208e5b9d63e960cf91a014e733a3b
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2019-02-27 19:16:48 +0100

    Version for new course

--- src/main/java/eu/wdaqua/wikidataPrivate/Main.java
+++ src/main/java/eu/wdaqua/wikidataPrivate/Main.java
@@ -1,6 +1,7 @@
 package eu.wdaqua.wikidataPrivate;
 
 
+import org.wikidata.wdtk.datamodel.helpers.Datamodel;
 import org.wikidata.wdtk.datamodel.helpers.ItemDocumentBuilder;
 import org.wikidata.wdtk.datamodel.helpers.StatementBuilder;
 import org.wikidata.wdtk.util.WebResourceFetcherImpl;
@@ -17,61 +18,69 @@ import java.util.List;
 
 
 public class Main {
-    final static String siteIri = "https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/index.php/";
+    final static String siteIri = "http://qanswer-svc1.univ-st-etienne.fr/index.php";
+    // final static String siteIri = "https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/index.php/";
 
     public static void main(String[] args) throws MediaWikiApiErrorException {
 
         WebResourceFetcherImpl.setUserAgent("Wikidata Toolkit EditOnlineDataExample");
 
-        ApiConnection con = new ApiConnection("https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/api.php");
+        ApiConnection con = new ApiConnection("http://qanswer-svc1.univ-st-etienne.fr/api.php");
 
         try {
             //Put in the first place the user with which you created the bot account
             //Put as password what you get when you create the bot account
-            con.login("Root", "D063520Bot@XXXXXXXXXXXXXX");
+            con.login("Admin", "my_bot@bia7riffnss8ujf108a0a67hs8qqmhl2");
         } catch (LoginFailedException e) {
             e.printStackTrace();
         }
 
 
 
-        //Example for getting information about an entity, here the example of The Laboratoire Huber Curien, Q900
+        //Example for getting information about an entity, here the example of Pierre Maret, Q1342
         //For more examples give a look at: https://github.com/Wikidata/Wikidata-Toolkit-Examples/blob/master/src/examples/FetchOnlineDataExample.java
         WikibaseDataFetcher wbdf = new WikibaseDataFetcher(con, siteIri);
-        ItemDocument laboratoireHC = (ItemDocument) wbdf.getEntityDocument("Q900");
+        ItemDocument laboratoireHC = (ItemDocument) wbdf.getEntityDocument("Q1342");
         System.out.println(laboratoireHC.getEntityId());
         System.out.println(laboratoireHC.getLabels());
         System.out.println(laboratoireHC.getStatementGroups());
         System.out.println(laboratoireHC.getDescriptions());
 
         //Example to search for an ID given the label
-        List<WbSearchEntitiesResult> entities = wbdf.searchEntities("Pierre Maret");
+        List<WbSearchEntitiesResult> entities = wbdf.searchEntities("France");
         for (WbSearchEntitiesResult entity : entities){
+            ItemDocument ent = (ItemDocument) wbdf.getEntityDocument(entity.getEntityId());
+            //ent.getStatementGroups().get(0).getStatements().get(0).getClaim().getValue().toString();
             System.out.println(entity.getEntityId());
         }
 
-        //Example for writing information about an entity, here the example of creating Florance Garrelie working at Laboratoire Huber Curien
+        //Example for writing information about an entity, here the example of creating MisterX living in Chambon Sur Lignon
         //For more examples give a look at: https://github.com/Wikidata/Wikidata-Toolkit-Examples/blob/master/src/examples/EditOnlineDataExample.java
         WikibaseDataEditor wbde = new WikibaseDataEditor(con, siteIri);
-        PropertyDocument propertyTravaille = (PropertyDocument) wbdf.getEntityDocument("P242");
+        PropertyDocument propertyTravaille = (PropertyDocument) wbdf.getEntityDocument("P580");
         System.out.println(propertyTravaille.getLabels());
 
         ItemIdValue noid = ItemIdValue.NULL; // used when creating new items
-        Statement statement1 = StatementBuilder
-                .forSubjectAndProperty(noid, propertyTravaille.getPropertyId())
-                .withValue(laboratoireHC.getItemId()).build();
+//        Statement statement1 = StatementBuilder
+//                .forSubjectAndProperty(noid, propertyTravaille.getPropertyId())
+//                .withValue(Datamodel.makeStringValue("bluuuu")).build();
+
+        StatementBuilder s = StatementBuilder.forSubjectAndProperty(noid, propertyTravaille.getPropertyId());
+        s.withValue(Datamodel.makeItemIdValue("Q2",siteIri));
+
+        Statement stat = s.build();
+
         ItemDocument itemDocument = ItemDocumentBuilder.forItemId(noid)
-                .withLabel("Florence Garrelie", "en")
-                .withLabel("Florence Garrelie", "fr")
-                .withStatement(statement1).build();
+                .withLabel("Mister X", "en")
+                .withLabel("Mister X", "fr")
+                .withStatement(stat).build();
         try {
             ItemDocument newItemDocument = wbde.createItemDocument(itemDocument,
                     "Statement created by our bot");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Created Florence Garrelie");
-        System.out.println("Created statement: Florence Garrelie travaille Laboratoire Huber Curien");
+        System.out.println("Created Mister X lives in chambon");
         System.out.println(itemDocument.getItemId().getId());
 
 
